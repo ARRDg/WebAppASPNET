a18acb143f12066dfd5b7c3cb30ef3359d0498a2
kirill <Defoult@gmail.com>
2025-02-24 23:10:47 +0300

    Добавил контроллер, модель для авторизации пользователя. Создал отдельный layout для авторизации.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppASPNET.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/RegisterModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppASPNET.Models
+{
+    public class RegisterModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public bool AgreeToTerms { get; set; }
+    }
+}
