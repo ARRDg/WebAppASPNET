3cefe25f8d1138755b24b0637669a66ed149c83c
kirill <Defoult@gmail.com>
2025-02-25 01:16:00 +0300

    Завершил настройку css для авторизации и регистрации

--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -5,7 +5,6 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string ConfirmPassword { get; set; }
         public bool AgreeToTerms { get; set; }
     }
 }
