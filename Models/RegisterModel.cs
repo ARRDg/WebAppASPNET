e5795171ab7c6c75442ea7fff362290b0ff0fdf9
kirill <Defoult@gmail.com>
2025-03-06 00:22:13 +0300

    Валидация для регистрации

--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -5,20 +5,26 @@ namespace WebAppASPNET.Models
     public class RegisterModel
     {
         [Required(ErrorMessage = "Name is required")]
-        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters")]
+        [StringLength(50, MinimumLength = 2,
+            ErrorMessage = "Name must be between 2 and 50 characters")]
+        [RegularExpression(@"^[a-zA-Z0-9-]+$",
+            ErrorMessage = "Name can only contain letters, numbers and hyphens")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Invalid email format")]
-        [StringLength(100, ErrorMessage = "Email cannot exceed 100 characters")]
+        [StringLength(100,
+            ErrorMessage = "Email cannot exceed 100 characters")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
-        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters")]
+        [StringLength(100, MinimumLength = 8,
+            ErrorMessage = "Password must be at least 8 characters")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "You must agree to the terms")]
+        [Range(typeof(bool), "true", "true",
+            ErrorMessage = "You must agree to the terms")]
         public bool AgreeToTerms { get; set; }
     }
 }
